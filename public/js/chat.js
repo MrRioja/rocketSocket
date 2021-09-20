4883c1710ea227f3ae39c37da7ae22eb519a8218
Luiz Rioja <lulyrioja@gmail.com>
2021-09-19 23:39:27 -0300

    :alien: Creating function to emit start chat event

---
+++ public/js/chat.js
@@ -0,0 +1,12 @@
+const socket = io("http://localhost:3333");
+
+function onLoad() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const name = urlParams.get("name");
+  const email = urlParams.get("email");
+  const avatar = urlParams.get("avatar");
+
+  socket.emit("start", { email, name, avatar });
+}
+
+onLoad();
