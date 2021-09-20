05dd658446a91b7c0542936ed42ad2b2dc6c0fd3
Luiz Rioja <lulyrioja@gmail.com>
2021-09-19 23:40:40 -0300

    :alien: Creating http and websocket server and creating database connection

---
+++ src/http.ts
@@ -0,0 +1,22 @@
+import "reflect-metadata";
+import express from "express";
+import path from "path";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+
+const app = express();
+
+const server = createServer(app);
+
+mongoose.connect("mongodb://localhost/rocketsocket");
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("Socket", socket.id);
+});
+
+export { server, io };
