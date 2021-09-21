f7b7c28c8e0376f24bdc11e7fe3e6185eab745a1
Luiz Rioja <lulyrioja@gmail.com>
2021-09-20 23:37:20 -0300

    :alien: Creating event to receive new users, retrieve registered users, and add users to the list

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,69 @@
 const socket = io("http://localhost:3333");
 
+let roomId = "";
+
 function onLoad() {
   const urlParams = new URLSearchParams(window.location.search);
   const name = urlParams.get("name");
   const email = urlParams.get("email");
   const avatar = urlParams.get("avatar");
 
-  socket.emit("start", { email, name, avatar });
+  document.querySelector(".user_logged").innerHTML += `
+    <img
+      class="avatar_user_logged"
+      src=${avatar}
+    />
+    <strong id="user_logged">${name}</strong>
+  `;
+
+  socket.emit("start", {
+    email,
+    name,
+    avatar,
+  });
+
+  socket.on("new_users", (user) => {
+    const existsInDiv = document.getElementById(`user_${user._id}`);
+    console.log(user);
+    if (!existsInDiv) {
+      addUser(user);
+    }
+  });
+
+  socket.emit("get_users", (users) => {
+    users.map((user) => {
+      if (user.email !== email) {
+        addUser(user);
+      }
+    });
+  });
 }
+
+function addUser(user) {
+  const usersList = document.getElementById("users_list");
+  usersList.innerHTML += `
+    <li
+      class="user_name_list"
+      id="user_${user._id}"
+      idUser="${user._id}"
+    >
+      <img
+        class="nav_avatar"
+        src=${user.avatar}
+      />
+      ${user.name}
+    </li>
+  `;
+}
+
+document.getElementById("users_list").addEventListener("click", (e) => {
+  if (e.target && e.target.matches("li.user_name_list")) {
+    const idUser = e.target.getAttribute("idUser");
+
+    socket.emit("start_chat", { idUser }, (data) => {
+      roomId = data.room.idChatRoom;
+    });
+  }
+});
 
 onLoad();
