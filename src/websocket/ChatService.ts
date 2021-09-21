95fd0857e89b24e8432a88167bc0bf6ae473ce12
Luiz Rioja <lulyrioja@gmail.com>
2021-09-20 23:36:06 -0300

    :alien: Creating an event to retrieve all registered users and an event to start chat

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,6 +1,11 @@
 import { container } from "tsyringe";
 import { io } from "../http";
+
+import { CreateChatRoomService } from "../services/CreateChatRoomService";
 import { CreateUserService } from "../services/CreateUserService";
+import { GetAllUsersService } from "../services/GetAllUsersService";
+import { GetUserBySocketIdService } from "../services/GetUserBySocketIdService";
+import { GetChatRoomByUserService } from "../services/GetChatRoomByUserService";
 
 io.on("connect", (socket) => {
   socket.on("start", async (data) => {
@@ -15,6 +20,39 @@ io.on("connect", (socket) => {
       socket_id: socket.id,
     });
 
-    console.log(user);
+    socket.broadcast.emit("new_users", user);
+  });
+
+  socket.on("get_users", async (callback) => {
+    const getAllUsersService = container.resolve(GetAllUsersService);
+    const users = await getAllUsersService.execute();
+
+    callback(users);
+  });
+
+  socket.on("start_chat", async (data, callback) => {
+    const createChatRoomService = container.resolve(CreateChatRoomService);
+    const getChatRoomByUsersService = container.resolve(
+      GetChatRoomByUserService
+    );
+    const getUserBySocketIdService = container.resolve(
+      GetUserBySocketIdService
+    );
+
+    const userLogged = await getUserBySocketIdService.execute(socket.id);
+
+    let room = await getChatRoomByUsersService.execute([
+      data.idUser,
+      userLogged?._id,
+    ]);
+
+    if (!room) {
+      room = await createChatRoomService.execute([
+        data.idUser,
+        userLogged?._id,
+      ]);
+    }
+
+    callback(room);
   });
 });
